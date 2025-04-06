6741e30ae94d39265bf14e3cd02b74cb9ba63dc0
AlirezaKhan7 <note4.alireza@gmail.com>
2025-04-06 21:34:20 +0330

    myEdit

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+  project_url TEXT := 'https://fgxgwcagpbnlwbsmpdvh.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZneGd3Y2FncGJubHdic21wZHZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4NjM5NjAsImV4cCI6MjA1OTQzOTk2MH0.YJ2HFiy12i81UeF_hhKhOKwsh0tRXwjHwca343_HYKg'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
