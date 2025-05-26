642abd0118cd11e3a8c36bf543bb42039a336863
AlirezaKhan7 <note4.alireza@gmail.com>
2025-05-26 11:16:40 +0330

    توضیح درباره تغییرات

--- components/chat/chat-secondary-buttons.tsx
+++ components/chat/chat-secondary-buttons.tsx
@@ -23,28 +23,6 @@ export const ChatSecondaryButtons: FC<ChatSecondaryButtonsProps> = ({}) => {
 
                 <div className="mx-auto mt-2 max-w-xs space-y-2 sm:max-w-sm md:max-w-md lg:max-w-lg">
                   <div>Model: {selectedChat.model}</div>
-                  <div>Prompt: {selectedChat.prompt}</div>
-
-                  <div>Temperature: {selectedChat.temperature}</div>
-                  <div>Context Length: {selectedChat.context_length}</div>
-
-                  <div>
-                    Profile Context:{" "}
-                    {selectedChat.include_profile_context
-                      ? "Enabled"
-                      : "Disabled"}
-                  </div>
-                  <div>
-                    {" "}
-                    Workspace Instructions:{" "}
-                    {selectedChat.include_workspace_instructions
-                      ? "Enabled"
-                      : "Disabled"}
-                  </div>
-
-                  <div>
-                    Embeddings Provider: {selectedChat.embeddings_provider}
-                  </div>
                 </div>
               </div>
             }
