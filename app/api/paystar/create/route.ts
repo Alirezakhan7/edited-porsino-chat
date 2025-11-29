a35a609be044365ed796bdda8f380ab18d895ac6
AlirezaKhan7 <note4.alireza@gmail.com>
2025-11-29 11:12:01 +0330

    آپدیت جدید پروژه

--- app/api/paystar/create/route.ts
+++ app/api/paystar/create/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: NextRequest) {
 
   try {
     // کاربر لاگین‌کرده
-    const supa = createServerClient(cookies())
+    const supa = createServerClient()
     const {
       data: { user }
     } = await supa.auth.getUser()
